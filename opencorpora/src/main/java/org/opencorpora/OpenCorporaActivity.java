00a314c2615205fc75e6cfe374dd648085eb1993
Roenke <roenke54@gmail.com>
2015-12-11 23:09:22 +0300

    add validation button to main activity

--- opencorpora/src/main/java/org/opencorpora/OpenCorporaActivity.java
+++ opencorpora/src/main/java/org/opencorpora/OpenCorporaActivity.java
@@ -1,5 +1,7 @@
 package org.opencorpora;
 
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -8,13 +10,16 @@ import android.view.View;
 import android.widget.TextView;
 
 import org.opencorpora.authenticator.AuthHelper;
+import org.opencorpora.authenticator.Authenticator;
 import org.opencorpora.authenticator.IAuthListener;
 
 
 public class OpenCorporaActivity extends Activity implements IAuthListener {
+    public Authenticator mAuthenticator;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mAuthenticator = new Authenticator(this);
         setContentView(R.layout.opencorpora_layout);
     }
 
@@ -44,5 +49,11 @@ public class OpenCorporaActivity extends Activity implements IAuthListener {
     public void onClick(View view){
         AuthHelper.getInstance().authorize("login", "password", this);
         AuthHelper.getInstance().subscribe(this);
+    }
+
+    public void tryAuth(View view){
+        Account[] accounts = AccountManager.get(this).getAccountsByType("org.opencorpora");
+        TextView textView = (TextView) findViewById(R.id.text_view);
+        textView.setText("Found: " + accounts.length + " opencorpora accounts.");
     }
 }
