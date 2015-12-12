ee32348953bfedfb76d4dde7039122bddf909a29
Roenke <roenke54@gmail.com>
2015-12-12 22:15:33 +0300

    remove async auth logic from main activity

--- opencorpora/src/main/java/org/opencorpora/OpenCorporaActivity.java
+++ opencorpora/src/main/java/org/opencorpora/OpenCorporaActivity.java
@@ -3,24 +3,22 @@ package org.opencorpora;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
-import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
-import org.opencorpora.authenticator.AuthHelper;
 import org.opencorpora.authenticator.Authenticator;
-import org.opencorpora.authenticator.IAuthListener;
 
 
-public class OpenCorporaActivity extends Activity implements IAuthListener {
-    public Authenticator mAuthenticator;
+public class OpenCorporaActivity extends Activity {
+    private Authenticator mAuthenticator;
+    private TextView mtTextView;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mAuthenticator = new Authenticator(this);
         setContentView(R.layout.opencorpora_layout);
+        mtTextView = (TextView) findViewById(R.id.text_view);
     }
 
     @Override
@@ -28,32 +26,12 @@ public class OpenCorporaActivity extends Activity implements IAuthListener {
         super.onStart();
     }
 
-    @Override
-    public void onSuccess() {
-        TextView textView = (TextView) findViewById(R.id.text_view);
-        SharedPreferences prefs = getSharedPreferences("AuthPrefs", MODE_PRIVATE);
-        String token = prefs.getString("token", "not found :(");
-        Log.d("prefs result", token);
-
-        textView.setText(getString(R.string.TokenReceivedPrefix) + token);
-        AuthHelper.getInstance().unSubscribe(this);
-    }
-
-    @Override
-    public void onFail() {
-        TextView textView = (TextView) findViewById(R.id.text_view);
-        textView.setText(R.string.RequestFailed);
-        AuthHelper.getInstance().unSubscribe(this);
-    }
-
     public void onClick(View view){
-        AuthHelper.getInstance().authorize("login", "password", this);
-        AuthHelper.getInstance().subscribe(this);
+        mtTextView.setText("None");
     }
 
     public void tryAuth(View view){
         Account[] accounts = AccountManager.get(this).getAccountsByType("org.opencorpora");
-        TextView textView = (TextView) findViewById(R.id.text_view);
-        textView.setText("Found: " + accounts.length + " opencorpora accounts.");
+        mtTextView.setText("Found: " + accounts.length + " opencorpora accounts.");
     }
 }
