471b7cd42d8ea83d91a721cc0efb70b9799c670f
Roenke <roenke54@gmail.com>
2015-12-13 14:00:56 +0300

    add manual sync button to main activity

--- opencorpora/src/main/java/org/opencorpora/OpenCorporaActivity.java
+++ opencorpora/src/main/java/org/opencorpora/OpenCorporaActivity.java
@@ -3,6 +3,7 @@ package org.opencorpora;
 import android.accounts.Account;
 import android.accounts.AccountManager;
 import android.app.Activity;
+import android.content.ContentResolver;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
@@ -24,7 +25,17 @@ public class OpenCorporaActivity extends Activity {
     }
 
     public void onClick(View view){
-        mTextView.setText("None");
+        Account[] accounts = AccountManager.get(this).getAccountsByType("org.opencorpora");
+        if(accounts.length > 0){
+            Bundle bundle = new Bundle();
+            bundle.putBoolean(ContentResolver.SYNC_EXTRAS_MANUAL, true);
+            bundle.putBoolean(ContentResolver.SYNC_EXTRAS_EXPEDITED, true);
+            ContentResolver.requestSync(accounts[0], "org.opencorpora.task.provider", bundle);
+            mTextView.setText("Sync is active? = " + ContentResolver.getSyncAdapterTypes().length);
+        }
+        else{
+            mTextView.setText("Sync types count = " + ContentResolver.getSyncAdapterTypes().length);
+        }
     }
 
     public void tryAuth(View view){
--- opencorpora/src/main/java/org/opencorpora/TaskService.java
+++ opencorpora/src/main/java/org/opencorpora/TaskService.java
@@ -8,12 +8,15 @@ import android.util.Log;
 public class TaskService extends Service {
     private static final String LOG_TAG = "TaskService";
 
+    private static final Object sSyncAdapterLock = new Object();
     private static TaskSyncAdapter sTaskSyncAdapter= null;
-    public TaskService() {
-        if(sTaskSyncAdapter == null){
-            sTaskSyncAdapter = new TaskSyncAdapter(getApplicationContext(), true);
+
+    @Override
+    public void onCreate() {
+        synchronized (sSyncAdapterLock) {
+            if (sTaskSyncAdapter == null)
+                sTaskSyncAdapter = new TaskSyncAdapter(getApplicationContext(), true);
         }
-        Log.d(LOG_TAG, "TaskService created");
     }
 
     @Override
