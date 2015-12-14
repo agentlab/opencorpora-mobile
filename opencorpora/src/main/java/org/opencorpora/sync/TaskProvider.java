782103c125aae89297b6dc221ba8e8eee16eb9fc
Roenke <roenke54@gmail.com>
2015-12-14 19:32:03 +0300

    make TaskProvider stub. Add NotNull annotation, add java doc for describe this

--- opencorpora/src/main/java/org/opencorpora/sync/TaskProvider.java
+++ opencorpora/src/main/java/org/opencorpora/sync/TaskProvider.java
@@ -4,21 +4,21 @@ import android.content.ContentProvider;
 import android.content.ContentValues;
 import android.database.Cursor;
 import android.net.Uri;
-import android.util.Log;
 
+import org.jetbrains.annotations.NotNull;
+
+/**
+ * Stub class only for data sync
+ * @see <a href="http://developer.android.com/intl/ru/training/sync-adapters/creating-stub-provider.html" >
+ */
 public class TaskProvider extends ContentProvider {
-    private static final String LOG_TAG = "TaskProvider";
-    public TaskProvider() {
-        Log.d(LOG_TAG, "TaskProviderCreated");
-    }
-
     @Override
     public boolean onCreate() {
         return false;
     }
 
     @Override
-    public Cursor query(Uri uri,
+    public Cursor query(@NotNull Uri uri,
                         String[] projection,
                         String selection,
                         String[] selectionArgs,
@@ -27,22 +27,22 @@ public class TaskProvider extends ContentProvider {
     }
 
     @Override
-    public String getType(Uri uri) {
+    public String getType(@NotNull Uri uri) {
         return null;
     }
 
     @Override
-    public Uri insert(Uri uri, ContentValues values) {
+    public Uri insert(@NotNull Uri uri, ContentValues values) {
         return null;
     }
 
     @Override
-    public int delete(Uri uri, String selection, String[] selectionArgs) {
+    public int delete(@NotNull Uri uri, String selection, String[] selectionArgs) {
         return 0;
     }
 
     @Override
-    public int update(Uri uri, ContentValues values, String selection, String[] selectionArgs) {
+    public int update(@NotNull Uri uri, ContentValues values, String selection, String[] selectionArgs) {
         return 0;
     }
 
