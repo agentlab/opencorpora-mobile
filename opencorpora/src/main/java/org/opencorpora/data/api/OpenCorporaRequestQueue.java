cb0f4fd590ba8055bf397f729b749171f31edde0
Roenke <roenke54@gmail.com>
2015-12-19 00:10:42 +0300

    add request class for volley library. Add Volley to build.gradle

---
+++ opencorpora/src/main/java/org/opencorpora/data/api/OpenCorporaRequestQueue.java
@@ -0,0 +1,41 @@
+package org.opencorpora.data.api;
+
+import android.content.Context;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+
+public class OpenCorporaRequestQueue {
+    private static OpenCorporaRequestQueue mInstance;
+    private static Context mContext;
+    private RequestQueue mRequestQueue;
+
+    private OpenCorporaRequestQueue(Context context){
+        mContext = context;
+        mRequestQueue = getRequestQueue();
+    }
+
+    public static synchronized OpenCorporaRequestQueue getInstance(Context context){
+        if(mInstance == null){
+            mInstance = new OpenCorporaRequestQueue(context);
+        }
+
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue(){
+        if(mRequestQueue == null){
+            Cache cache = new DiskBasedCache(mContext.getCacheDir(), 1024 * 1024);
+            Network network = new BasicNetwork(new HurlStack());
+            mRequestQueue = new RequestQueue(cache, network);
+            // Don't forget to start the volley request queue
+            mRequestQueue.start();
+        }
+
+        return mRequestQueue;
+    }
+}
